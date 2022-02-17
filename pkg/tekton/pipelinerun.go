0cfb2f2a305caf0ff86b7af7ebfd0e4d46270c70
Vincent Demeester <vdemeest@redhat.com>
2022-02-17 17:19:19 +0100

    Refactoring pkg/tekton to reduce duplication
    - Split into several small files
    - Re-use step to llb translation
    
    Next step will be unit tests, a bit more "functionnal".
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

---
+++ pkg/tekton/pipelinerun.go
@@ -0,0 +1,77 @@
+package tekton
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/buildkit/frontend/gateway/client"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+)
+
+func PipelineRunToLLB(ctx context.Context, c client.Client, pr *v1beta1.PipelineRun) (llb.State, error) {
+	// Validation
+	if pr.Name == "" && pr.GenerateName != "" {
+		pr.Name = pr.GenerateName + "generated"
+	}
+	if err := pr.Validate(ctx); err != nil {
+		return llb.State{}, errors.Wrapf(err, "validation failed for PipelineRun %s", pr.Name)
+	}
+	if pr.Spec.PipelineSpec == nil {
+		return llb.State{}, errors.New("PipelineRef not supported")
+	}
+
+	// Interpolation
+	// TODO(vdemeester) implement this
+
+	// Execution
+	pipelineWorkspaces := map[string]llb.MountOption{}
+	for _, w := range pr.Spec.PipelineSpec.Workspaces {
+		pipelineWorkspaces[w.Name] = llb.AsPersistentCacheDir(pr.Name+"/"+w.Name, llb.CacheMountShared)
+	}
+	logrus.Infof("pipelineWorkspaces: %+v", pipelineWorkspaces)
+	tasks := map[string][]llb.State{}
+	for _, t := range pr.Spec.PipelineSpec.Tasks {
+		logrus.Infof("pipelinetask: %s", t.Name)
+		if t.TaskSpec == nil {
+			return llb.State{}, errors.Errorf("%s: TaskRef not supported", t.Name)
+		}
+		taskWorkspaces := map[string]llb.MountOption{}
+		for _, w := range t.Workspaces {
+			taskWorkspaces["/workspace/"+w.Name] = pipelineWorkspaces[w.Workspace]
+		}
+		logrus.Infof("taskWorkspaces: %+v", taskWorkspaces)
+		steps, err := taskSpecToPSteps(ctx, c, t.TaskSpec.TaskSpec, t.Name, taskWorkspaces)
+		if err != nil {
+			return llb.State{}, errors.Wrap(err, "couldn't translate TaskSpec to llb")
+		}
+		logrus.Infof("steps: %+v", steps)
+		mounts := []llb.RunOption{}
+		if len(t.RunAfter) > 0 {
+			// RunAfter means, the first steps of the current Task needs to start after the last step of the referenced Task
+			// We are going to use mounts here too.
+			for _, a := range t.RunAfter {
+				targetMount := fmt.Sprintf("/tekton/from-task/%s", a)
+				mounts = append(mounts,
+					llb.AddMount(targetMount, tasks[a][len(tasks[a])-1], llb.SourcePath("/tekton/results"), llb.Readonly),
+				)
+			}
+		}
+		stepStates, err := pstepToState(c, steps, mounts)
+		if err != nil {
+			return llb.State{}, err
+		}
+		tasks[t.Name] = stepStates
+	}
+	ft := llb.Scratch()
+	fa := llb.Mkdir("/task", os.FileMode(int(0777)))
+	for n, t := range tasks {
+		state := t[len(t)-1]
+		taskPath := fmt.Sprintf("/task/%s", n)
+		fa = fa.Copy(state, "/tekton", taskPath, &llb.CopyInfo{FollowSymlinks: true, CreateDestPath: true, AllowWildcard: true, AllowEmptyWildcard: true})
+	}
+	return ft.File(fa, llb.WithCustomName("[results] buildking image from result (fake)"), llb.IgnoreCache), nil
+}
--- pkg/tekton/tekton.go
+++ pkg/tekton/tekton.go
@@ -3,14 +3,11 @@ package tekton
 import (
 	"context"
 	"fmt"
-	"os"
 	"strings"
 
-	"github.com/docker/distribution/reference"
 	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/frontend/gateway/client"
 	ocispecs "github.com/opencontainers/image-spec/specs-go/v1"
-	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	k8scheme "k8s.io/client-go/kubernetes/scheme"
@@ -53,139 +50,12 @@ func TektonToLLB(c client.Client) func(context.Context, string) (llb.State, erro
 		}
 
 		if len(types.TaskRuns) > 0 {
-			return taskRunToLLB(ctx, c, types.TaskRuns[0])
+			return TaskRunToLLB(ctx, c, types.TaskRuns[0])
 		} else if len(types.PipelineRuns) > 0 {
-			return pipelineRunToLLB(ctx, c, types.PipelineRuns[0])
+			return PipelineRunToLLB(ctx, c, types.PipelineRuns[0])
 		}
 		return llb.State{}, fmt.Errorf("Invalid state")
 	}
-}
-
-func taskRunToLLB(ctx context.Context, c client.Client, tr *v1beta1.TaskRun) (llb.State, error) {
-	if tr.Name == "" && tr.GenerateName != "" {
-		tr.Name = tr.GenerateName + "generated"
-	}
-	if err := tr.Validate(ctx); err != nil {
-		return llb.State{}, err
-	}
-	steps, err := taskSpecToSteps(ctx, c, *tr.Spec.TaskSpec)
-	return steps[len(steps)-1].s, err
-}
-
-func taskSpecToSteps(ctx context.Context, c client.Client, t v1beta1.TaskSpec) ([]step, error) {
-	steps := make([]step, len(t.Steps))
-	for i, s := range t.Steps {
-		logrus.Infof("step: %s\n", s.Name)
-		ref, err := reference.ParseNormalizedNamed(s.Image)
-		if err != nil {
-			return steps, errors.Wrapf(err, "failed to parse stage name %q", s.Image)
-		}
-		logrus.Infof("ref %v", ref)
-		ts := llb.Image(ref.String(), llb.WithMetaResolver(c), llb.WithCustomName("load metadata from "+ref.String()))
-		// TODO: support script (how?)
-		runOpt := []llb.RunOption{
-			llb.Args(append(s.Command, s.Args...)),
-			// llb.Dir("/dest"), // FIXME: support workdir
-			llb.IgnoreCache, // FIXME: see if we can enable the cache on some run
-		}
-		if s.WorkingDir != "" {
-			runOpt = append(runOpt, llb.With(llb.Dir(s.WorkingDir)))
-		}
-		mounts := []llb.RunOption{
-			llb.AddMount("/tekton/results", steps[i].s, llb.AsPersistentCacheDir("results", llb.CacheMountShared)),
-		}
-		if i > 0 {
-			// TODO: mount previous results or something to create a dependency
-			targetMount := fmt.Sprintf("/tekton-results/%d", i-1)
-			mounts = append(mounts,
-				llb.AddMount(targetMount, steps[i-1].s, llb.SourcePath("/tekton/results"), llb.Readonly),
-			)
-		}
-		step := step{}
-		step.s = ts.Run(append(runOpt, mounts...)...).Root()
-		steps[i] = step
-	}
-	return steps, nil
-}
-
-func pipelineRunToLLB(ctx context.Context, c client.Client, pr *v1beta1.PipelineRun) (llb.State, error) {
-	tasks := map[string]task{}
-
-	if pr.Name == "" && pr.GenerateName != "" {
-		pr.Name = pr.GenerateName + "generated"
-	}
-	if err := pr.Validate(ctx); err != nil {
-		return llb.State{}, err
-	}
-	pipelineWorkspaces := map[string]llb.MountOption{}
-	for _, w := range pr.Spec.PipelineSpec.Workspaces {
-		pipelineWorkspaces[w.Name] = llb.AsPersistentCacheDir(pr.Name+"/"+w.Name, llb.CacheMountShared)
-	}
-	logrus.Infof("pipelineWorkspaces: %+v", pipelineWorkspaces)
-	for _, t := range pr.Spec.PipelineSpec.Tasks {
-		logrus.Infof("task: %s\n", t.Name)
-		steps := make([]step, len(t.TaskSpec.TaskSpec.Steps))
-		taskWorkspaces := map[string]llb.MountOption{}
-		for _, w := range t.Workspaces {
-			taskWorkspaces["/workspace/"+w.Name] = pipelineWorkspaces[w.Workspace]
-		}
-		logrus.Infof("taskWorkspaces: %+v", taskWorkspaces)
-		for j, s := range t.TaskSpec.TaskSpec.Steps {
-			logrus.Infof("step: %s\n", s.Name)
-			name := fmt.Sprintf("[%s/%s]", t.Name, s.Name)
-			// TODO: support script (how?)
-			runOpt := []llb.RunOption{
-				llb.Args(append(s.Command, s.Args...)),
-				// llb.Dir("/dest"), // FIXME: support workdir
-				llb.IgnoreCache, // FIXME: see if we can enable the cache on some run
-				llb.WithCustomName(name),
-			}
-			if s.WorkingDir != "" {
-				runOpt = append(runOpt, llb.With(llb.Dir(s.WorkingDir)))
-			}
-			mounts := []llb.RunOption{
-				llb.AddMount("/tekton/results", steps[j].s, llb.AsPersistentCacheDir("results", llb.CacheMountShared)),
-			}
-			for path, mountOpt := range taskWorkspaces {
-				mounts = append(mounts,
-					llb.AddMount(path, steps[j].s, mountOpt),
-				)
-			}
-			if j > 0 {
-				// TODO: mount previous results or something to create a dependency
-				targetMount := fmt.Sprintf("/tekton-results/%d", j-1)
-				mounts = append(mounts,
-					llb.AddMount(targetMount, steps[j-1].s, llb.SourcePath("/tekton/results"), llb.Readonly),
-				)
-			}
-			if len(t.RunAfter) > 0 {
-				// RunAfter means, the first steps of the current Task needs to start after the last step of the referenced Task
-				// We are going to use mounts here too.
-				for _, a := range t.RunAfter {
-					targetMount := fmt.Sprintf("/tekton/from-task/%s", a)
-					mounts = append(mounts,
-						llb.AddMount(targetMount, tasks[a].steps[len(tasks[a].steps)-1].s, llb.SourcePath("/tekton/results"), llb.Readonly),
-					)
-				}
-			}
-			logrus.Infof("mounts: %+v", mounts)
-			step := step{}
-			step.s = llb.Image(s.Image, llb.WithMetaResolver(c)).Run(append(runOpt, mounts...)...).Root()
-			steps[j] = step
-		}
-		tasks[t.Name] = task{
-			steps: steps,
-		}
-		logrus.Infof("tasks: %+v", tasks)
-	}
-	ft := llb.Scratch()
-	fa := llb.Mkdir("/task", os.FileMode(int(0777)))
-	for n, t := range tasks {
-		state := t.steps[len(t.steps)-1].s
-		taskPath := fmt.Sprintf("/task/%s", n)
-		fa = fa.Copy(state, "/tekton", taskPath, &llb.CopyInfo{FollowSymlinks: true, CreateDestPath: true, AllowWildcard: true, AllowEmptyWildcard: true})
-	}
-	return ft.File(fa, llb.WithCustomName("[results] buildking image from result (fake)"), llb.IgnoreCache), nil
 }
 
 func readTypes(data string) types {
